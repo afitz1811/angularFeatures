071ca53204e8d13e370e82dd2cbf405551227693
afitz1811 <afitzsimons94@googlemail.com>
2022-07-26 15:09:10 +0100

    Angular first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { InterpolationComponent } from './interpolation/interpolation.component';
+import { NotfoundComponent } from './notfound/notfound.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'interpolation', component: InterpolationComponent},
+  { path: '', component: HomeComponent, pathMatch: 'full'},
+  { path: '**', component: NotfoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { InterpolationComponent } from './interpolation/interpolation.component';
+import { NotfoundComponent } from './notfound/notfound.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    InterpolationComponent,
+    NotfoundComponent
   ],
   imports: [
     BrowserModule,
