1269613badcb9fe0c56f370655270b1f611accfa
afitz1811 <afitzsimons94@googlemail.com>
2022-07-26 16:14:42 +0100

    Interpolation commit

--- src/app/interpolation/interpolation.component.ts
+++ src/app/interpolation/interpolation.component.ts
@@ -12,4 +12,19 @@ export class InterpolationComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  title = "Interpolation";
+  classColour = 'green';
+  imageURL = "https://img.freepik.com/premium-photo/kitten-head-with-paw-up-peeking-blank-white-sign-placard-pet-kitten-curiously-peeking-white-background-tabby-baby-cat-showing-placard-template-long-web-banner-with-copy-space_221542-2279.jpg?w=1380";
+  script ='<script>alert("You are hacked")</script>';
+  div='<div>this is a div</div>';
+
+
+  getTitle() : string {
+    return this.title;
+  }
+
+  max(first: number, second: number) : number {
+    return Math.max(first, second);
+  }
+
 }
